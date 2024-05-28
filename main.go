636ce88dbec07268d9ff1a58bfc61d0334947c62
PhantomHydrino <55507011+PhantomHydrino@users.noreply.github.com>
2024-05-28 20:00:51 +0530

    refactor: created utils package
    moved urlshortener.go to utils package

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/PhantomHydrino/go-url-shortener/utils"
 )
 
 func main() {
-	http.HandleFunc("/", ShortenURLHandler)
+	http.HandleFunc("/", utils.ShortenURLHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
